b4d253387221253f060b2eaf0f8f244c3ab0d752
Larwive <larwive@gmail.com>
2024-07-26 11:21:57 +0800

    Variable refactoring in `edf_to_dat.py`

--- edf_to_dat.py
+++ edf_to_dat.py
@@ -16,19 +16,22 @@ sub_count = 1
 for dirpath, dirnames, filenames in os.walk('./RBDdata/'):
     for filename in filenames:
         if filename.endswith(".edf"):
-            print("Processing {}".format(dirpath.split('/')[-1]))
+            subject = dirpath.split('/')[-1]
+            print("Processing {}".format(subject))
             raw = mne.io.read_raw_edf(os.path.join(dirpath, filename), verbose=0, preload=True)
             raw = preprocess_raw(raw)
             sampling_rate = raw.info['sfreq']
+            label = dirpath.split('/')[-2]
+            date = os.path.splitext(filename)[0]
 
-            csvfile = pd.read_csv(os.path.join(dirpath, os.path.splitext(filename)[0] + ".csv"))
+            csvfile = pd.read_csv(os.path.join(dirpath, date + ".csv"))
             mask = csvfile[' Sleep Stage'].str.contains('R')
 
             groups = mask.ne(mask.shift()).cumsum()
 
             filtered_blocks = csvfile.loc[mask].groupby(groups).agg(list)
             if len(filtered_blocks['Manual Epoch']) == 0:
-                print('No REM for {}'.format(dirpath.split('/')[-1]), file=sys.stderr)
+                print('No REM for {}'.format(subject), file=sys.stderr)
                 continue
             print("REM blocks:")
             for phase, indices in enumerate(filtered_blocks['Manual Epoch']):
@@ -37,9 +40,9 @@ for dirpath, dirnames, filenames in os.walk('./RBDdata/'):
                 end = int(indices[-1] * sampling_rate * 30)
                 data_dict = {
                     "data": np.array(raw.get_data(start=start, stop=end)),
-                    "labels": [int(dirpath.split('/')[2])],
+                    "labels": [int(label)],
                 }
-                new_name = "s{}-{}_{}-{}({}).dat".format(sub_count, dirpath.split('/')[-1], os.path.splitext(filename)[0], phase, dirpath.split('/')[-2])
+                new_name = "s{}-{}_{}-{}({}).dat".format(sub_count, subject, date, phase, label)
                 with open(os.path.join('./RBDdata/dat/', new_name), 'wb') as f:
                     pickle.dump(data_dict, f)
             sub_count += 1
